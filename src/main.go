3b964fe97a88e50534f8b1b3cb3cb781c94bfbc4
Amadou Toure <amadouelcapotoure@gmail.com>
2025-10-03 16:20:22 -0400

    add format

--- src/main.go
+++ src/main.go
@@ -7,76 +7,77 @@ import (
 	"path/filepath"
 	utils "stemExtractor/utils"
 
+	//models "stemExtractor/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/google/uuid"
 )
-
-
 
 func main() {
 	app := fiber.New(
 		fiber.Config{
-		Prefork:       false,
-		CaseSensitive: true,
-		StrictRouting: true,
-		BodyLimit:     1024 * 1024 * 1024, //1GB
-		ServerHeader:  "Fiber"})
+			Prefork:       false,
+			CaseSensitive: true,
+			StrictRouting: true,
+			BodyLimit:     1024 * 1024 * 1024, //1GB
+			ServerHeader:  "Fiber"})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:8081, http://127.0.0.1:8081",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowHeaders: "*",
+	}))
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		utils.CompressToZip("../storage/separated/htdemucs/Starship Syncopation - Cory Wong","../storage/zipped","starship-syncopation")
 		return c.SendString("Hello, World!")
 	})
-	app.Get("/status/:id",func (c *fiber.Ctx) error {
-		jobId:=c.Params("id")
-		
-		status:=utils.GetJobStatus(jobId)
+
+	app.Get("/status/:id", func(c *fiber.Ctx) error {
+		jobId := c.Params("id")
+
+		status := utils.GetJobStatus(jobId)
 
 		return c.Status(200).SendString(status)
 	})
-	
 
 	app.Get("/download/:id", func(c *fiber.Ctx) error {
-	    jobId := c.Params("id")
-	    filePath := fmt.Sprintf("../storage/zipped/%s.zip", jobId)
+		jobId := c.Params("id")
+		filePath := fmt.Sprintf("../storage/zipped/%s.zip", jobId)
 
-	    f, err := os.Open(filePath)
-	    if err != nil {
-	        return c.Status(500).SendString("Erreur ouverture fichier: " + err.Error())
-	    }
+		f, err := os.Open(filePath)
+		if err != nil {
+			return c.Status(500).SendString("Erreur ouverture fichier: " + err.Error())
+		}
 
-	    fileInfo, err := f.Stat()
-	    if err != nil {
-	        f.Close()
-	        return c.Status(500).SendString("Erreur récupération info fichier: " + err.Error())
-	    }
+		fileInfo, err := f.Stat()
+		if err != nil {
+			f.Close()
+			return c.Status(500).SendString("Erreur récupération info fichier: " + err.Error())
+		}
 
-	    c.Set("Content-Type", "application/zip")
-	    c.Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.zip\"", jobId))
-	    c.Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
-
-	    return c.SendStream(f)
+		c.Set("Content-Type", "application/zip")
+		c.Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.zip\"", jobId))
+		c.Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
+		fmt.Println("remote host started a download")
+		return c.Status(200).SendStream(f)
+		//return c.Status(200).SendFile(filePath)
 	})
-	
 
-	app.Post("/unmix",func(c *fiber.Ctx)error{
-		jobId:= uuid.New().String()
+	app.Post("/unmix", func(c *fiber.Ctx) error {
+		jobId := uuid.New().String()
 		//reception et formattage du fichier
 		fileHeader, err := c.FormFile("file")
 		if err != nil {
-			return c.Status(400).SendString(fmt.Sprintf("Erreur lecture fichier: %v", err))
+			fmt.Println("Erreur lecture fichier:", err)
+			return c.Status(400).SendString("Erreur lecture fichier:" + err.Error())
 		}
 		savePath := fmt.Sprintf("../storage/uploads/%s", jobId+filepath.Ext(fileHeader.Filename))
 		err = c.SaveFile(fileHeader, savePath)
-		if (err!=nil){
-			return c.Status(500).SendString("erreur pendant la sauvegarde: "+err.Error())
+		if err != nil {
+			return c.Status(500).SendString("erreur pendant la sauvegarde: " + err.Error())
 		}
-		
-		go utils.UseDemucs(jobId)//lancement de l'unmixing
-	return c.Status(200).SendString("job id:"+jobId)
-		//sauvegarde dans le repertoire
 
-		
-
-	// return c.SendFile("../storage/zipped/"+stemDirName+".zip")
+		go utils.UseDemucs(jobId) //lancement de l'unmixing
+		return c.Status(200).SendString(jobId)
 	})
 	app.Listen(":3000")
 }
--- src/utils/unmix.go
+++ src/utils/unmix.go
@@ -7,35 +7,32 @@ import (
 	"path/filepath"
 )
 
-
-
-
 func UseDemucs(jobId string) error {
 	//execution de la separation
-	uploadFolder,err:=filepath.Abs("../storage/uploads")
+	uploadFolder, err := filepath.Abs("../storage/uploads")
 	if err != nil {
-		return fmt.Errorf("Erreur obtention du chemin courant:"+err.Error())
+		return fmt.Errorf("Erreur obtention du chemin courant:" + err.Error())
 	}
-	resultFolder,err:=filepath.Abs("../storage/separated")
+	resultFolder, err := filepath.Abs("../storage/separated")
 	if err != nil {
 		return fmt.Errorf("Erreur obtention du chemin courant: " + err.Error())
 	}
-	SetJobStatus("pending",jobId)
-	cmd := exec.Command("docker", "run", "--rm" , "-v", fmt.Sprintf("%s:/app", uploadFolder), "-v", fmt.Sprintf("%s:/app/separated", resultFolder), "demucs-cpu", "demucs", "/app/"+jobId+".mp3")
+	SetJobStatus("pending", jobId)
+	cmd := exec.Command("docker", "run", "--rm", "-v", fmt.Sprintf("%s:/app", uploadFolder), "-v", fmt.Sprintf("%s:/app/separated", resultFolder), "demucs-cpu", "demucs", "/app/"+jobId+".mp3")
 	// Récupérer la sortie
 	output, err := cmd.CombinedOutput()
 	fmt.Println(string(output))
-	if (err != nil){
-		return fmt.Errorf("erreur durant la separation:"+string(output))
+	if err != nil {
+		return fmt.Errorf("erreur durant la separation:" + string(output))
 	}
 	//compression de des pistes
 
-	err=CompressToZip(resultFolder+"/htdemucs","../storage/zipped",jobId)
-	if err != nil{
-		SetJobStatus("error",jobId)
+	err = CompressToZip(resultFolder+"/htdemucs", "../storage/zipped", jobId)
+	if err != nil {
+		SetJobStatus("error", jobId)
 	}
-	SetJobStatus("donne",jobId)
-	defer os.RemoveAll("../storage/separated/htdemucs/"+jobId)
-	defer os.RemoveAll("../storage/uploads/"+jobId+".mp3")
+	SetJobStatus("done", jobId)
+	defer os.RemoveAll("../storage/separated/htdemucs/" + jobId)
+	defer os.RemoveAll("../storage/uploads/" + jobId + ".mp3")
 	return nil
 }
